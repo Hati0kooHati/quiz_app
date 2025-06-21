5d9fd9cdfa029caf180f10c32fb1939dda3beb09
Hati0kooHati <dserion.prn@gmail.com>
2025-06-21 14:30:08 +0600

    Update main.dart

--- quiz app 1.0/lib/main.dart
+++ quiz app 1.0/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:quiz_app/quiz_app.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(QuizApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (fn) {
+    runApp(QuizApp());
+    }
+  );
 }
