56492d3335c1d89d4f259d39465bbba8f6c9e785
Hati0kooHati <dserion.prn@gmail.com>
2025-06-21 14:38:05 +0600

    Update result_screen.dart

--- quiz app 1.0/lib/screens/result_screen.dart
+++ quiz app 1.0/lib/screens/result_screen.dart
@@ -3,12 +3,12 @@ import 'package:quiz_app/services/functions.dart';
 
 import 'package:flutter/material.dart';
 
+
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("$correctAndTappedAnswers");
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
